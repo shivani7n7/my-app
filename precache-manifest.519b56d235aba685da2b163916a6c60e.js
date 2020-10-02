0bacc9d7f310469a601c83b5e651f6180193318e
shivani7n7 <shivanimehta7n7@gmail.com>
2020-10-03 01:46:15 +0530

    Updates

---
+++ precache-manifest.519b56d235aba685da2b163916a6c60e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32fcfdee1bb79d257b6f1a7fe5b425a",
+    "url": "/shivani7n7/my-app/index.html"
+  },
+  {
+    "revision": "dad1c1459215c74d1713",
+    "url": "/shivani7n7/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/shivani7n7/my-app/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shivani7n7/my-app/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad1c1459215c74d1713",
+    "url": "/shivani7n7/my-app/static/js/main.8cce9f93.chunk.js"
+  },
+  {
+    "revision": "7d16fb31eb387f07fd19",
+    "url": "/shivani7n7/my-app/static/js/runtime-main.96c5f15f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shivani7n7/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
